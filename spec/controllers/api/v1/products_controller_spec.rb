bd344ff0083ac66fdd2298e812a7e05e54a86d6e
Dan <chandleryang76@gmail.com>
2017-08-08 06:29:20 +1200

    first commit

---
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -0,0 +1,258 @@
+require 'rails_helper'
+
+
+describe Api::V1::ProductsController do
+
+  describe "GET #show" do
+    before(:each) do
+      @product = FactoryGirl.create :product
+      get :show, id: @product.id
+    end
+
+    it "returns the information about a reporter on a hash" do
+      product_response = json_response
+      expect(product_response[:title]).to eql @product.title
+    end
+
+    it "has the user as a embeded object" do
+      product_response = json_response
+      expect(product_response[:user][:email]).to eql @product.user.email
+    end
+
+    it { should respond_with 200 }
+  end
+
+  describe "GET #index" do
+    before(:each) do
+      4.times { FactoryGirl.create :product }
+    end
+
+    context "when is not receiving any product_ids parameter" do
+      before(:each) do
+        get :index
+      end
+
+      it "returns 4 records from the database" do
+        products_response = json_response
+        expect(products_response).to have(4).items
+      end
+
+      it "returns the user object into each product" do
+        products_response = json_response
+        products_response.each do |product_response|
+          expect(product_response[:user]).to be_present
+        end
+      end
+
+      it { should respond_with 200 }
+    end
+
+    context "when product_ids parameter is sent" do
+      before(:each) do
+        @user = FactoryGirl.create :user
+        3.times { FactoryGirl.create :product, user: @user }
+        get :index, product_ids: @user.product_ids
+      end
+
+      it "returns just the products that belong to the user" do
+        products_response = json_response
+        products_response.each do |product_response|
+          expect(product_response[:user][:email]).to eql @user.email
+        end
+      end
+    end
+
+
+  end
+
+  describe "POST #create" do
+    context "when is successfully created" do
+      before(:each) do
+        user = FactoryGirl.create :user
+        @product_attributes = FactoryGirl.attributes_for :product
+        api_authorization_header user.auth_token
+        post :create, { user_id: user.id, product: @product_attributes }
+      end
+
+      it "renders the json representation for the product record just created" do
+        product_response = json_response
+        expect(product_response[:title]).to eql @product_attributes[:title]
+      end
+
+      it { should respond_with 201 }
+    end
+
+    context "when is not created" do
+      before(:each) do
+        user = FactoryGirl.create :user
+        @invalid_product_attributes = { title: "Smart TV", price: "Twelve dollars" } #notice I'm not including the email
+        api_authorization_header user.auth_token
+        post :create, { user_id: user.id, product: @invalid_product_attributes }
+      end
+
+      it "renders an errors json" do
+        product_response = json_response
+        expect(product_response).to have_key(:errors)
+      end
+
+      it "renders the json errors on whye the user could not be created" do
+        product_response = json_response
+        expect(product_response[:errors][:price]).to include "is not a number"
+      end
+
+      it { should respond_with 422 }
+    end
+  end
+
+  describe "PUT/PATCH #update" do
+    before(:each) do
+      @user = FactoryGirl.create :user
+      @product = FactoryGirl.create :product, user: @user
+      api_authorization_header @user.auth_token
+    end
+
+    context "when is successfully updated" do
+      before(:each) do
+        patch :update, { user_id: @user.id, id: @product.id, product: { title: "An expensive TV" } }
+      end
+
+      it "renders the json representation for the updated user" do
+        product_response = json_response
+        expect(product_response[:title]).to eql "An expensive TV"
+      end
+
+      it { should respond_with 200 }
+    end
+
+    context "when is not updated" do
+      before(:each) do
+        patch :update, { user_id: @user.id, id: @product.id, product: { price: "two hundred" } }
+      end
+
+      it "renders an errors json" do
+        product_response = json_response
+        expect(product_response).to have_key(:errors)
+      end
+
+      it "renders the json errors on whye the user could not be created" do
+        product_response = json_response
+        expect(product_response[:errors][:price]).to include "is not a number"
+      end
+
+      it { should respond_with 422 }
+    end
+  end
+
+  describe "DELETE #destroy" do
+    before(:each) do
+      @user = FactoryGirl.create :user
+      @product = FactoryGirl.create :product, user: @user
+      api_authorization_header @user.auth_token
+      delete :destroy, { user_id: @user.id, id: @product.id }
+    end
+
+    it { should respond_with 204 }
+  end
+
+  describe ".filter_by_title" do
+    before(:each) do
+      @product1 = FactoryGirl.create :product, title: "A plasma TV"
+      @product2 = FactoryGirl.create :product, title: "Fastest Laptop"
+      @product3 = FactoryGirl.create :product, title: "CD player"
+      @product4 = FactoryGirl.create :product, title: "LCD TV"
+
+    end
+
+    context "when a 'TV' title pattern is sent" do
+      it "returns the 2 products matching" do
+        expect(Product.filter_by_title("TV")).to have(2).items
+      end
+
+      it "returns the products matching" do
+        expect(Product.filter_by_title("TV").sort).to match_array([@product1, @product4])
+      end
+    end
+  end
+
+  describe ".above_or_equal_to_price" do
+    before(:each) do
+      @product1 = FactoryGirl.create :product, price: 100
+      @product2 = FactoryGirl.create :product, price: 50
+      @product3 = FactoryGirl.create :product, price: 150
+      @product4 = FactoryGirl.create :product, price: 99
+    end
+
+    it "returns the products which are above or equal to the price" do
+      expect(Product.above_or_equal_to_price(100).sort).to match_array([@product1, @product3])
+    end
+  end
+
+
+  describe ".below_or_equal_to_price" do
+    before(:each) do
+      @product1 = FactoryGirl.create :product, price: 100
+      @product2 = FactoryGirl.create :product, price: 50
+      @product3 = FactoryGirl.create :product, price: 150
+      @product4 = FactoryGirl.create :product, price: 99
+    end
+
+    it "returns the products which are above or equal to the price" do
+      expect(Product.below_or_equal_to_price(99).sort).to match_array([@product2, @product4])
+    end
+  end
+
+
+  describe ".recent" do
+    before(:each) do
+      @product1 = FactoryGirl.create :product, price: 100
+      @product2 = FactoryGirl.create :product, price: 50
+      @product3 = FactoryGirl.create :product, price: 150
+      @product4 = FactoryGirl.create :product, price: 99
+
+      #we will touch some products to update them
+      @product2.touch
+      @product3.touch
+    end
+
+    it "returns the most updated records" do
+      expect(Product.recent).to match_array([@product3, @product2, @product4, @product1])
+    end
+  end
+
+  describe ".search" do
+    before(:each) do
+      @product1 = FactoryGirl.create :product, price: 100, title: "Plasma tv"
+      @product2 = FactoryGirl.create :product, price: 50, title: "Videogame console"
+      @product3 = FactoryGirl.create :product, price: 150, title: "MP3"
+      @product4 = FactoryGirl.create :product, price: 99, title: "Laptop"
+    end
+
+    context "when title 'videogame' and '100' a min price are set" do
+      it "returns an empty array" do
+        search_hash = { keyword: "videogame", min_price: 100 }
+        expect(Product.search(search_hash)).to be_empty
+      end
+    end
+
+    context "when title 'tv', '150' as max price, and '50' as min price are set" do
+      it "returns the product1" do
+        search_hash = { keyword: "tv", min_price: 50, max_price: 150 }
+        expect(Product.search(search_hash)).to match_array([@product1])
+      end
+    end
+
+    context "when an empty hash is sent" do
+      it "returns all the products" do
+        expect(Product.search({})).to match_array([@product1, @product2, @product3, @product4])
+      end
+    end
+
+    context "when product_ids is present" do
+      it "returns the product from the ids" do
+        search_hash = { product_ids: [@product1.id, @product2.id]}
+        expect(Product.search(search_hash)).to match_array([@product1, @product2])
+      end
+    end
+  end
+
+end
